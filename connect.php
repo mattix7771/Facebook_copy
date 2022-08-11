5ba71a2d86507662bd3dd1de0d9160c1c5a787ae
NicoleSM <nicolina.husky@gmail.com>
2022-08-11 10:24:51 +0200

    connecting myphpadmin server

--- connect.php
+++ connect.php
@@ -1,12 +1,13 @@
 <?php
-/*
+
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     //Database connection
-    $conn ? new mysqli('localhost','root','','fb');
+    $conn = new mysqli('localhost','root','','fb');
     if($conn->connect_error){
-        die('Connection failed: ' $conn->connect_error)
+        echo "$conn->connect_error";
+        die("Connection failed: ". $conn->connect_error);
     }
     else{
         $stmt = $conn->prepare("insert into test(username, password) values(?,?)");
@@ -14,33 +15,7 @@
         $stmt->execute();
         echo "Operation successful";
         $stmt->close();
-        $conn-close();
-    }*//*
-
-    // database connection code
-// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-
-$con = mysqli_connect('localhost', 'root', '','fb');
-
-// get the post records
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-// database insert SQL code
-$sql = "INSERT INTO `test` (`username`, `password`) VALUES ($username, $password)";
-
-// insert in database 
-$rs = mysqli_query($con, $sql);
-
-if($rs)
-{
-	echo "Contact Records Inserted";
-}*/
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-echo "hdioewf";
+        $conn->close();
+    }
 
 ?>
