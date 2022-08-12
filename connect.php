895b21be7b86261a5e380482c6ba37ce1e0e6a79
NicoleSM <nicolina.husky@gmail.com>
2022-08-12 23:59:41 +0200

    finalized db connection and improved design

--- connect.php
+++ connect.php
@@ -4,6 +4,12 @@
     $l_name = $_POST['l_name'];
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $day = $_POST['bd_day'];
+    $month = $_POST['bd_month'];
+    $year = $_POST['bd_year'];
+    #$date = $day.",".$month.",".$year; 
+    $date = $year.",".$day.",".$month; 
+    $gender = $_POST['gender'];
 
     //Database connection
     $conn = new mysqli('localhost','root','','fb');
@@ -12,8 +18,8 @@
         die("Connection failed: ". $conn->connect_error);
     }
     else{
-        $stmt = $conn->prepare("insert into credentials(f_name, l_name, username, pass) values(?,?,?,?)");
-        $stmt->bind_param("ssss",$f_name, $l_name, $username, $password);
+        $stmt = $conn->prepare("insert into credentials(f_name, l_name, username, pass, birthday, gender) values(?,?,?,?,?,?)");
+        $stmt->bind_param("ssssss", $f_name, $l_name, $username, $password, $date, $gender);
         $stmt->execute();
         echo "Operation successful";
         $stmt->close();
